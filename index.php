f63a87b78ba5b667dfa6f9ff128d16ab2f531187
keyur2714 <keyurjava27@gmail.com>
2023-06-10 09:13:44 +0530

    Updated with CSS

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		
 	</head>
 	<body>
-		<p style="color:gree;">Hello Radhe Krishna...</p>
+		<p style="color:green;">Hello Radhe Krishna...</p>
 		<h1 style="color:blue;">Hello From PHP...</h1>
 	</body>
 </html>
